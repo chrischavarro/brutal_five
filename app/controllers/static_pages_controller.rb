628c5b0c67ef4fcb52c8f1f20de9e94c0d6248eb
Christian Chavarro <christian@clutchprep.com>
2017-03-25 15:08:42 -0400

    Username attribute now saves to user model upon signing up

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,11 +1,19 @@
 class StaticPagesController < ApplicationController
+
+
 	def home
 		# @idea = Idea.new(
 		# 	premise: params[:premise]
 		# 	)
 
 		# session[:idea] = @idea.premise
+		if user_signed_in?
+			redirect_to dashboard_path
+		end
+	end
 
+	def dashboard
+		@user = current_user
 	end
 
 	def ideas_test
--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,5 @@ Rails.application.routes.draw do
   root to: 'static_pages#home'
 
   get '/ideas_test' => 'static_pages#ideas_test'
+  get '/dashboard' => 'static_pages#dashboard'
 end
