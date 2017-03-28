f72dc443cb2ded59db6d3ea5a7f8b88e12e4eb88
Christian Chavarro <christian@clutchprep.com>
2017-03-28 19:26:43 -0400

    Included bootstrap js to project'

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,14 +1,19 @@
 class ProfilesController < ApplicationController
 	def new
-		@profile = Profile.new
+		# TODO ON 3/27: FINISH CREATING AND ASSIGNING PROFILE TO USER, ALONG WITH PUSHING CATEGORY TO PROFILE
+		# 
+		@profile = Profile.new(
+			user_id: current_user.id
+			)
+		@categories = Category.all
 		@user = current_user
 		@idea = @user.ideas.last
 	end
 
 	def create
-		@profile = Profile.new(
-			user_id = current_user.id 
-			)
-		@profile.save
+		# @profile = Profile.new(
+		# 	user_id = current_user.id 
+		# 	)
+		# @profile.save
 	end
 end
